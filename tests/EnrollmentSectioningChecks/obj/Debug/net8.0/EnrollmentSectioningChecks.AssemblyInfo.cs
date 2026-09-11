56033b486f2753def34af8ed36545bcde56f1087
Cookies-Dookies <pascualpatrick264@gmail.com>
2026-09-11 10:29:32 +0800

    reset workflows

--- tests/EnrollmentSectioningChecks/obj/Debug/net8.0/EnrollmentSectioningChecks.AssemblyInfo.cs
+++ tests/EnrollmentSectioningChecks/obj/Debug/net8.0/EnrollmentSectioningChecks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnrollmentSectioningChecks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd2867c9ef8280bf083f622516f2faf28de4e47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f582e308686ffac353e87a27a5c9aa92b8bac198")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnrollmentSectioningChecks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnrollmentSectioningChecks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
