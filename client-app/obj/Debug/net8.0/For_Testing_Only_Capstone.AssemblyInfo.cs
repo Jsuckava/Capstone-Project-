d9a8640b86446b6f799d91e276b3c6e5e7ed0c9a
Cookies-Dookies <pascualpatrick264@gmail.com>
2026-09-11 11:04:14 +0800

    configuration for CXML

--- client-app/obj/Debug/net8.0/For_Testing_Only_Capstone.AssemblyInfo.cs
+++ client-app/obj/Debug/net8.0/For_Testing_Only_Capstone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("For_Testing_Only_Capstone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f582e308686ffac353e87a27a5c9aa92b8bac198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46055a5d26f59c00e0c9c7eca129a21bf97cf032")]
 [assembly: System.Reflection.AssemblyProductAttribute("For_Testing_Only_Capstone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("For_Testing_Only_Capstone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
